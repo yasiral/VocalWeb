fb3f30f67ef14e7e5bfc731dda44aa97eb598a60
yasiral <yasir.altaf.ab@gmail.com>
2025-03-19 11:17:44 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 # CLI/standalone entry point
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import argparse
 from src.services.text_extraction import fetch_and_display_content
 from src.services.language_detection import detect_language
